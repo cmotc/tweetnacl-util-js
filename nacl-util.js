b126903b0eddcd87962142f7c8ebd4ece452429a
idk <idk@devuan.local>
2016-08-07 18:47:14 -0400

    added base 32 encoding decoding functions. Pretty sure that's what I need to talk to Crypto::Sodium.

--- nacl-util.js
+++ nacl-util.js
@@ -25,6 +25,26 @@
     return decodeURIComponent(escape(s.join('')));
   };
 
+  util.encodeBase32 = function(arr) {
+    if (typeof btoa === 'undefined') {
+      return (new Buffer(arr)).toString('base32');
+    } else {
+      var i, s = [], len = arr.length;
+      for (i = 0; i < len; i++) s.push(String.fromCharCode(arr[i]));
+      return btoa(s.join(''));
+    }
+  };
+
+  util.decodeBase32 = function(s) {
+    if (typeof atob === 'undefined') {
+      return new Uint8Array(Array.prototype.slice.call(new Buffer(s, 'base32'), 0));
+    } else {
+      var i, d = atob(s), b = new Uint8Array(d.length);
+      for (i = 0; i < d.length; i++) b[i] = d.charCodeAt(i);
+      return b;
+    }
+  };
+
   util.encodeBase64 = function(arr) {
     if (typeof btoa === 'undefined') {
       return (new Buffer(arr)).toString('base64');
